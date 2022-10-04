7e9691187d24103a885420c906534bdc3ac7262d
Nima Nassehi <n.nassehi@intellisports.net>
2022-10-04 08:55:36 -0400

    Request data on load - Singleton

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,22 +9,33 @@ import XCTest
 
 
 class RemoteFeedLoader {
-
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url")
+    }
 }
 
 class  HTTPClient {
-
+    static let shared = HTTPClient()
+    
+    private init() {}
     var requestedURL : URL?
 }
 
-class RemoteFeedLoaderTests : XCTest {
+class RemoteFeedLoaderTests : XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         _ = RemoteFeedLoader()
-        let client = HTTPClient()
-        
+        let client = HTTPClient.shared
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let sut = RemoteFeedLoader()
+        let client = HTTPClient.shared
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
