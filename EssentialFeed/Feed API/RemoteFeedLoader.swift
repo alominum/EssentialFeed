7f4547a8f3f4915fb7ce8094e041574f7709a3da
Nima Nassehi <n.nassehi@intellisports.net>
2022-10-11 21:51:59 -0400

    Move mapping logic into `FeedLoaderMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,29 +39,42 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map {$0.item}))
-                } else {
+                do {
+                    completion(.success(try FeedItemMapper.map(data, response)))
+                } catch {
                     completion(.failure(.invalidData))
                 }
+
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
     }
     
-    private struct Root : Decodable {
-        let items : [Item]
-    }
-    
-    private struct Item : Decodable {
-        let id : UUID
-        let location: String?
-        let description : String?
-        let image : URL
+    private class FeedItemMapper {
+        static func map(_ data : Data, _ response : HTTPURLResponse) throws -> [FeedItem] {
+            if response.statusCode == 200,let root = try? JSONDecoder().decode(Root.self, from: data) {
+                return root.items.map{$0.item}
+            } else {
+                throw RemoteFeedLoader.Error.invalidData
+            }
+        }
         
-        var item : FeedItem {
-            return FeedItem(id: id, location: location, description: description, imageURL: image)
+        private struct Root : Decodable {
+            let items : [Item]
+        }
+        
+        private struct Item : Decodable {
+            let id : UUID
+            let location: String?
+            let description : String?
+            let image : URL
+            
+            var item : FeedItem {
+                return FeedItem(id: id, location: location, description: description, imageURL: image)
+            }
         }
     }
+    
+
 }
