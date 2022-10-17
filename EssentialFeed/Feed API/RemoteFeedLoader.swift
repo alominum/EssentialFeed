f8f3a9471ef4876ba29446c4cde05e344aacd55b
Nima Nassehi <n.nassehi@intellisports.net>
2022-10-17 09:00:30 -0400

    Use the 'LoadFeedResult' to prepare for conforming to protocol

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -16,10 +16,7 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
-    public enum Result : Equatable{
-        case success([FeedItem])
-        case failure(Error)
-    }
+    public typealias Result = LoadFeedResult<Error>
     
     public init (url : URL, client : HTTPClient) {
         self.client = client
@@ -34,7 +31,7 @@ public final class RemoteFeedLoader {
             case let .success(data, response):
                 completion(FeedItemMapper.map(data, response))
             case .failure:
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
         }
     }
